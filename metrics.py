0cdcd0a8a865e65d5a54ab29ddd5ee440737ce9a
castellonmayra <castellonmayra@gmail.com>
2022-08-09 18:17:20 -0400

    import metrics data from csv

---
+++ metrics.py
@@ -0,0 +1,16 @@
+from pandas import read_csv
+from statistics import mean, median
+import os
+import csv
+
+result = read_csv("metricsdata.csv")
+
+
+
+#  "https://github.com/castellonmayra/mc-freestyle/blob/main/metricsdata.csv"
+
+print(result)
+
+
+
+
