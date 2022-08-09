affbc4fa2f5af3c8fb4e6c47dec37e115f31e896
castellonmayra <castellonmayra@gmail.com>
2022-08-09 18:27:32 -0400

    1st data viz - market value

--- metrics.py
+++ metrics.py
@@ -3,14 +3,41 @@ from statistics import mean, median
 import os
 import csv
 
-result = read_csv("metricsdata.csv")
+result_df = read_csv("metricsdata.csv")
 
 
 
 #  "https://github.com/castellonmayra/mc-freestyle/blob/main/metricsdata.csv"
 
-print(result)
+print(type(result_df))
+#print(result_df)
+print(result_df.columns)
+print(len(result_df))
+
+print(result_df.head(3))
+
+#####
+
+mv_col = result_df["MARKET VALUE"]
+print(mv_col)
+
+print(mv_col.max())
 
 
 
+###
 
+from plotly.express import bar
+
+fig = bar(data_frame=result_df, 
+            y="SECURITY", 
+            x="MARKET VALUE",
+            orientation="h",
+            title="Market Value By Security", 
+            
+            # from the docs: "The keys of this dict should correspond to column names, 
+            # and the values should correspond to the desired label to be displayed.
+            labels={"SECURITY": "Security", "MARKET VALUE": "Market Value"},
+          
+            color="MARKET VALUE")
+fig.show()
